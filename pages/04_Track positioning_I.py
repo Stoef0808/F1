95fdf240dcc5b695c12514bd70494021f11d8b63
ilyesbenie <59012123+ilyesbenie@users.noreply.github.com>
2022-11-25 10:15:30 +0100

    Try new plot

--- pages/04_Track positioning_I.py
+++ pages/04_Track positioning_I.py
@@ -24,3 +24,37 @@ fig.update_layout(
 )
 st.plotly_chart(fig)
 
+fig, ax = plt.subplots(sharex=True, sharey=True, figsize=(12, 6.75))
+#fig.suptitle(f'{weekend.name} {year} - {driver} - Speed', size=24, y=0.97)
+
+plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.12)
+ax.axis('off')
+
+# After this, we plot the data itself.
+# Create background track line
+ax.plot(test['world_position_X'] , test['world_position_Y'] , color='black', linestyle='-', linewidth=15, zorder=0)
+
+# Create a continuous norm to map from data points to colors
+norm = plt.Normalize(color.min(), color.max())
+lc = LineCollection(segments, cmap=colormap, norm=norm, linestyle='-', linewidth=5)
+
+# Set the values used for colormapping
+lc.set_array(color)
+
+# Merge all line segments together
+line = ax.add_collection(lc)
+
+
+# Finally, we create a color bar as a legend.
+cbaxes = fig.add_axes([0.25, 0.05, 0.5, 0.05])
+normlegend = mpl.colors.Normalize(vmin=color.min(), vmax=color.max())
+legend = mpl.colorbar.ColorbarBase(cbaxes, norm=normlegend, cmap=colormap, orientation="horizontal")
+
+
+#for row in ax:
+ #   for col in row:
+  #      col.plot(x, y)
+
+# Show the plot
+#plt.show()
+st.plotly_chart(fig)
