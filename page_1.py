3bb259c15d06f9c1b8e708c68f82e53c31c448fe
ilyesbenie <59012123+ilyesbenie@users.noreply.github.com>
2022-12-02 11:04:34 +0100

    Update page_1.py
    changed title and icon

--- page_1.py
+++ page_1.py
@@ -4,14 +4,14 @@ import numpy
 
 
 st.set_page_config(
-    page_title="Ex-stream-ly Cool App",
-    page_icon="🧊",
+    page_title="F1 Telemetry App",
+    page_icon="🏎️",
 )
 
 st.title("Data Driven Business Lab")
 st.subheader("F1 project")
 st.markdown("""
-Welcome to the DDBL F1 project.
+Welcome to the DDBL F1 project. On this page you can upload your CSV files. 
 """)
 
 
