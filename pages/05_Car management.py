79880f2c58abacba4bd88919ca83e6fe77a5be03
Stoef0808 <121017744+Stoef0808@users.noreply.github.com>
2022-12-19 12:42:41 +0100

    Update 05_Car management.py

--- pages/05_Car management.py
+++ pages/05_Car management.py
@@ -24,7 +24,28 @@ fig.update_layout(
     height=1000,
     autosize=False,
     margin=dict(t=0, b=0, l=0, r=0),
-    template="plotly_white")
+updatemenus=[
+       dict(
+            buttons=list([
+                dict(
+                    args=[{'visible': [True, True, True, True,True,True,True,True]}],
+                    label="All tyres",
+                    method="update"
+                ),
+                dict(
+                    args=[{'visible': [True, True, True, True,False,False,False,False]}],
+                    label="All tyres player 1",
+                    method="update"
+                ),
+                dict(
+                    args=[{'visible': [False, False, False, False, True, True, True, True]}],
+                    label="All tyres player 2",
+                    method="update"
+                ),
+            ]),
+        ),   
+    ]
+)
 
 st.plotly_chart(fig)
 #@@
@@ -43,14 +64,39 @@ fig.add_trace(go.Scatter(x=pl1['totalDistance'], y=pl1['Right rear tyre temp'], 
 fig.add_trace(go.Scatter(x=pl1['totalDistance'], y=pl1['Left rear tyre temp'], name='Left rear'))
 fig.add_trace(go.Scatter(x=pl1['totalDistance'], y=pl1['Right front tyre temp'], name='Right front'))
 fig.add_trace(go.Scatter(x=pl1['totalDistance'], y=pl1['Left front tyre temp'], name='Left front'))
+fig.add_trace(go.Scatter(x=pl1['totalDistance'], y=pl2['Right rear tyre temp'], name='Right rear'))
+fig.add_trace(go.Scatter(x=pl1['totalDistance'], y=pl2['Left rear tyre temp'], name='Left rear'))
+fig.add_trace(go.Scatter(x=pl1['totalDistance'], y=pl2['Right front tyre temp'], name='Right front'))
+fig.add_trace(go.Scatter(x=pl1['totalDistance'], y=pl2['Left front tyre temp'], name='Left front'))
 fig.update_layout(
-    title='Tyre Deg player 1',
+    title='Tyre temp',
     xaxis_title='Distance lap',
     yaxis_title='Tyre deg',
     width=1200,
     height=1000,
     autosize=False,
     margin=dict(t=0, b=0, l=0, r=0),
-    template="plotly_white")
+    updatemenus=[
+       dict(
+            buttons=list([
+                dict(
+                    args=[{'visible': [True, True, True, True,True,True,True,True]}],
+                    label="All tyres",
+                    method="update"
+                ),
+                dict(
+                    args=[{'visible': [True, True, True, True,False,False,False,False]}],
+                    label="All tyres player 1",
+                    method="update"
+                ),
+                dict(
+                    args=[{'visible': [False, False, False, False, True, True, True, True]}],
+                    label="All tyres player 2",
+                    method="update"
+                ),
+            ]),
+        ),   
+    ]
+)
 
 st.plotly_chart(fig)
